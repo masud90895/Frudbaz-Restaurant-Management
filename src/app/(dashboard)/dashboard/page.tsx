4fb5500ae67f0fec2d7491109144902a8fb56df0
masud90895 <masudhossainmbs129@gmail.com>
2023-12-01 14:24:00 +0600

    history page updated

--- src/app/(dashboard)/dashboard/page.tsx
+++ src/app/(dashboard)/dashboard/page.tsx
@@ -162,7 +162,7 @@ export default function Checkout() {
                           id="country"
                           name="country"
                           autoComplete="country"
-                          className="block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                          className="block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-primary/60 focus:border-primary/60 sm:text-sm"
                         >
                           <option>Bangladesh</option>
                           <option>Canada</option>
@@ -202,7 +202,7 @@ export default function Checkout() {
                                 checked
                                   ? "border-transparent"
                                   : "border-gray-300",
-                                active ? "ring-2 ring-indigo-500" : "",
+                                active ? "ring-2 ring-primary/60" : "",
                                 "relative flex cursor-pointer rounded-lg border bg-white p-4 shadow-sm focus:outline-none"
                               )
                             }
@@ -241,7 +241,7 @@ export default function Checkout() {
                                   className={classNames(
                                     active ? "border" : "border-2",
                                     checked
-                                      ? "border-indigo-500"
+                                      ? "border-primary/60"
                                       : "border-transparent",
                                     "pointer-events-none absolute -inset-px rounded-lg"
                                   )}
